8f413a91b062bb939522212f91fefd9abed5d938
adzashel <seigneur.noir15@gmai.com>
2025-06-10 08:11:59 +0700

    Bubble Sort and Bead Sort java

---
+++ src/Library_FIne.java
@@ -0,0 +1,20 @@
+public class Library_FIne {
+    public static void main(String[] args) {
+        int d1 = 5 , d2 = 17, m1 = 8,  m2 = 8, y1 = 2025 , y2 = 2025;
+
+        System.out.println(CalculateFine(d1 , d2 , m1 , m2 , y1 , y2));
+    }
+
+    public static int CalculateFine(int d1 , int d2 , int m1 , int m2 , int y1 , int y2) {
+        if(y1 > y2) return 10000;
+
+        if(y1 == y2 ) {
+            if(m1 > m2 ) return (500*(m1 - m2));
+
+            if((m1 == m2 ) && (d1 > d2 )) {
+                return (15 * (d1 - d2));
+            }
+        }
+        return 0;
+    }
+}
