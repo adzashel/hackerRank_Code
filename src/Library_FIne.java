9aa1307ee057b16438c9f50babfcdf9e57c74870
adzashel <seigneur.noir15@gmai.com>
2025-06-10 08:13:04 +0700

    New Question Solved

--- src/Library_FIne.java
+++ src/Library_FIne.java
@@ -1,6 +1,6 @@
 public class Library_FIne {
     public static void main(String[] args) {
-        int d1 = 5 , d2 = 17, m1 = 8,  m2 = 8, y1 = 2025 , y2 = 2025;
+        int d1 = 26 , d2 = 17, m1 = 8,  m2 = 8, y1 = 2025 , y2 = 2025;
 
         System.out.println(CalculateFine(d1 , d2 , m1 , m2 , y1 , y2));
     }
@@ -14,6 +14,7 @@ public class Library_FIne {
             if((m1 == m2 ) && (d1 > d2 )) {
                 return (15 * (d1 - d2));
             }
+
         }
         return 0;
     }
